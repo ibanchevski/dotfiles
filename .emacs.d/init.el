5a8c8f91433523226245ea5f8082d48076b4bc71
Ivaylo Banchevski <ivaylo.banchevski@gmail.com>
2021-01-15 01:13:37 +0200

    Clean and reorganize emacs config

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -7,30 +7,49 @@
 ;; You may delete these explanatory comments.
 (package-initialize)
 
-(load-theme 'gruvbox-dark-hard)
-(global-display-line-numbers-mode)
+;; Default theme
+(load-theme 'solarized-wombat-dark t)
 
-;; init auto-complete
-(ac-config-default)
+;; default font
+(set-face-attribute 'default nil
+                    :family "Hack"
+                    :height 110
+                    :weight 'normal
+                    :width 'normal)
 
-;; Projectile keymaps and init
+;; (global-display-line-numbers-mode)
+(setq-default truncate-lines -1)
+
+;; Ident with spaces
+(set-default 'indent-tabs-mode nil)
+
+;; Disable auto-save
+(auto-save-mode -1)
+
+;; Remove all bars and scrolls
+(menu-bar-mode -1)
+(toggle-scroll-bar -1)
+(tool-bar-mode -1)
+
+;; Set visual bell
+(setq ring-bell-function 'ignore)
+
+;; Selectrum
+(selectrum-mode +1)
+
+;; Projectile
 (require 'projectile)
 (define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
-(define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
 (define-key projectile-mode-map (kbd "C-x p") 'projectile-command-map)
-(setq projectile-project-search-path '("~/repositories"))
 (setq projectile-indexing-method 'alien)
+(setq projectile-project-search-path '("~/repositories/" "/var/www/html/"))
 (projectile-mode +1)
 
-;; Remove the menu bar in terminal mode
-(menu-bar-mode -1)
+;; Flycheck
+(global-flycheck-mode +1)
 
 ;; store all backup and autosave files in the tmp dir
+(setq backup-directory-alist `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms `((".*" ,temporary-file-directory t)))
 
-(setq backup-directory-alist
-      
-      `((".*" . ,temporary-file-directory)))
-
-(setq auto-save-file-name-transforms
-      
-      `((".*" ,temporary-file-directory t))
+;;; init.el ends here
